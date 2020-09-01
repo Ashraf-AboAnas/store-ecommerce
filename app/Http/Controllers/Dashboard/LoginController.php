5eeb17caee147b9dc5b6a8b4a2de645a74892e89
Ashraf-AboAnas <ashraf.moh.azam@gmail.com>
2020-09-01 04:30:49 +0300

    End First branch 1/9/2020

---
+++ app/Http/Controllers/Dashboard/LoginController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\AdminLoginRequest; //to validation
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+   public function login (){
+     return view('dashboard.auth.login');
+
+   }
+
+   public function postlogin (AdminLoginRequest $request){
+ // return $request; // check if function goto  success rout In action form  action="{{route('admin.postlogin')}}.
+ $remember_me = $request->has('remember_me') ? true : false;
+
+        if (auth()->guard('admin')->attempt(['email' => $request->input("email"), 'password' => $request->input("password")], $remember_me)) {
+           // notify()->success('تم الدخول بنجاح  ');
+           //return'hhhhhhhhhhhhhhhhhhhhhh'; // test
+            return redirect() -> route('admin.dashboard');
+        }
+       // notify()->error('خطا في البيانات  برجاء المجاولة مجدا ');
+        return redirect()->back()->with(['error' => 'هناك خطا بالبيانات']);
+
+    }
+}
--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,7 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
-
+use Illuminate\Support\Facades\Request;
 class Authenticate extends Middleware
 {
     /**
@@ -14,8 +14,11 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
+        if (! $request->expectsJson()) { //if not rigester in admin table (not Auth)
+            if (Request::is('admin/*')) //if Route admin/* but not Auth or not rigester in admin table
+            return route('admin.login');// redirect to admin.login
+            else
+            return route('login'); // redirect to user.login
         }
     }
 }
